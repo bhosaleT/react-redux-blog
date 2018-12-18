ac3130b3f8af21fc0486ac8a846b6a68a72cefb1
Tejas Bhosale <bhosaletejas93@gmail..com>
2018-12-18 12:54:13 +0530

    using redux thunk middleware to make async action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
-    return {
-        type: "FETCH_POSTS"
-    };
-};
+import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+
+//action creator for fetching all the posts using the middleware redux thunk.
+export const fetchPosts = () =>
+    async dispatch => {
+        const response = await jsonPlaceHolder.get('/posts');
+        dispatch({
+            type: 'FETCH_POSTS',
+            payload: response
+        })
+    }
